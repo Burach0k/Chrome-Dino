f03743dd183a35680eae632021cbaa9fea2c4bcc
Andrey Burak <burak_andrej@mail.ru>
2020-07-11 14:12:30 +0300

    fix: fix bug with update row

--- row/row.c
+++ row/row.c
@@ -3,23 +3,27 @@
 typedef struct Row {
     void (*startt)(struct Row *, SDL_Renderer *);
     int rowWidth;
-    int size;
-    int *rowMas;
-    int step;
+    int pictureSize;
+    int *firstRow;
+    int *secondRow;
+    int stepForFirstRow;
+    int stepForSecondRow;
+    int rowHeight;
+    int rowStep;
 } Row;
 
-static void createNewRow(struct Row *row) {
+static void createNewRow(struct Row *row, int * focusRow) {
     for(int x = 0; x < row->rowWidth / 20; x++) {
         int getRandomRow = rand() % (3 - 1 + 1) + 1;
 
-        for (int y = 0; y < 8; y++) {
+        for (int y = 0; y < row->rowHeight; y++) {
             for (int w = 0; w < 20; w++) {
                 if (getRandomRow == 1) {
-                    *(row->rowMas + x*20 + y * row->rowWidth + w) = row1[y][w];
+                    *(focusRow + x*20 + y * row->rowWidth + w) = row1[y][w];
                 } else if (getRandomRow == 2) {
-                    *(row->rowMas + x*20 + y * row->rowWidth + w) = row2[y][w];
+                    *(focusRow + x*20 + y * row->rowWidth + w) = row2[y][w];
                 } else if (getRandomRow == 3) {
-                    *(row->rowMas + x*20 + y * row->rowWidth + w) = row3[y][w];
+                    *(focusRow + x*20 + y * row->rowWidth + w) = row3[y][w];
                 }
             }
         }
@@ -29,36 +33,47 @@ static void createNewRow(struct Row *row) {
 static void startt(struct Row *row, SDL_Renderer* renderer) {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-    drowPicture(renderer, row->rowMas, -row->step, 160, row->size, row->rowWidth, 8);
+    drowPicture(renderer, row->firstRow, row->stepForFirstRow, 160, row->pictureSize, row->rowWidth, row->rowHeight);
+    drowPicture(renderer, row->secondRow, row->stepForSecondRow, 160, row->pictureSize, row->rowWidth, row->rowHeight);
 
-    if (row->step == row->rowWidth / 2) {
-
-        createNewRow(row);
-
-        row->step = 0;
+    if (row->stepForFirstRow == - row->rowWidth) {
+        createNewRow(row, row->firstRow);
+        row->stepForFirstRow = row->rowWidth;
     }
 
-    row->step++;
+    if (row->stepForSecondRow == - row->rowWidth) {
+        createNewRow(row, row->secondRow);
+        row->stepForSecondRow = row->rowWidth;
+    }
+
+    row->stepForFirstRow--;
+    row->stepForSecondRow--;
 }
 
-Row* new_Row(int windowWidth, int size) {
+Row* new_Row(int windowWidth, int pictureSize) {
     Row *row = NULL;
     row = malloc(sizeof(Row));
 
-    int koefGostWidth = 2;
-    int rowWidth =  windowWidth / size * koefGostWidth + 1;
+    int rowWidth =  windowWidth / pictureSize + 1;
 
-    int *rowMas = NULL;
+    int *firstRow = NULL;
+    int *secondRow = NULL;
 
-    rowMas = malloc(rowWidth * 8 * sizeof(int));
+    row->rowHeight = 15;
+    firstRow = malloc(rowWidth * row->rowHeight * sizeof(int));
+    secondRow = malloc(rowWidth * row->rowHeight * sizeof(int));
 
-    row->step = 0;
-    row->rowMas = rowMas;
+    row->stepForFirstRow = 0;
+    row->stepForSecondRow = rowWidth;
+    row->rowStep = 1;
+    row->firstRow = firstRow;
+    row->secondRow = secondRow;
     row->rowWidth = rowWidth;
-    row->size = size;
+    row->pictureSize = pictureSize;
     row->startt = startt;
 
-    createNewRow(row);
+    createNewRow(row, row->firstRow);
+    createNewRow(row, row->secondRow);
 
     return row;
 }
